8927c327725cd03bc12781c1feca017ba75e07ed
zhanghui <zhanghui@donview.cn>
2019-04-17 17:25:12 +0800

    1.好像不错哎

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '萌萌哒小可爱', 
+    description: '这是谁的小鹿？真可爱!',
+    // head: [
+    //     ['link', { rel: 'icon', href: '/img/logo.ico' }],
+    //     ['link', { rel: 'manifest', href: '/manifest.json' }],
+    // ]
+}
