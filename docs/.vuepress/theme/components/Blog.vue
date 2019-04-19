42b96222680ddd6741ea4d13056b2b457d21d924
zhanghui <zhanghui@donview.cn>
2019-04-19 14:15:55 +0800

    1.怪我，没看清楚文档，罪过

---
+++ docs/.vuepress/theme/components/Blog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 过滤后宫,留言板 这些 -->
+    <div v-for="(data,index) in datas">
+      <article>
+        <div class="lovely-title">{{data.title}}</div>
+        <div class="lovely-content-box" v-if="data.excerpt">
+          <div class="lovely-content" v-html="data.excerpt"></div>
+          <div>查看更多</div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blog",
+  computed: {
+    datas() {
+      this.init();
+      return this.$site.pages;
+    }
+  },
+  methods: {
+    init() {}
+  }
+};
+</script>
+
+<style lang="stylus"></style>
--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -6,13 +6,6 @@
         :src="$withBase(data.heroImage)"
         :alt="data.heroAlt || 'hero'"
       >
-
-      <h1 v-if="data.heroText !== null" id="main-title">{{ data.heroText || $title || 'Hello' }}</h1>
-
-      <p class="description">
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-      </p>
-
       <p
         class="action"
         v-if="data.actionText && data.actionLink"
@@ -23,23 +16,7 @@
         />
       </p>
     </header>
-
-    <div
-      class="features"
-      v-if="data.features && data.features.length"
-    >
-      <div
-        class="feature"
-        v-for="(feature, index) in data.features"
-        :key="index"
-      >
-        <h2>{{ feature.title }}</h2>
-        <p>{{ feature.details }}</p>
-      </div>
-    </div>
-
-    <Content class="custom"/>
-
+    <blog></blog>
     <div
       class="footer"
       v-if="data.footer"
@@ -51,9 +28,10 @@
 
 <script>
 import NavLink from '@theme/components/NavLink.vue'
+import Blog from './Blog'
 
 export default {
-  components: { NavLink },
+  components: { NavLink,Blog },
 
   computed: {
     data () {
