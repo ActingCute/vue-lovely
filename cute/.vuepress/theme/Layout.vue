2b4d32b3dcd979240b3cfea68398df5f27add45b
zhanghui <zhanghui@donview.cn>
2019-06-04 17:05:38 +0800

    1.提问:build时候怎么将数据直接输出静态页面？

---
+++ cute/.vuepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="theme-container">
+        <Content />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cute",
+        mounted(){
+            console.log("okokok --- ",this)
+        }
+    }
+</script>
