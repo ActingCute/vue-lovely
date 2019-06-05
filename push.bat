d496fa35e0b0d7c9a6e69c59ebe716973151f38d
小莹草 <rem486@qq.com>
2019-06-05 16:26:50 +0800

    1.back

---
+++ push.bat
@@ -0,0 +1,6 @@
+echo off
+if not exist dist\html (
+    echo no build file
+    exit
+)
+cd %~dp0dist\html & git add . & git commit -m "update --" & git push -f git@www.haibarai.com:/var/repo/blog.git
