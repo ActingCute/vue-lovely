c6a4abc4c78851b7ca089b06358e72d83de53158
小莹草 <rem486@qq.com>
2019-06-05 12:59:02 +0800

    back --

--- htmlPush.bat
+++ htmlPush.bat
@@ -3,4 +3,5 @@ if not exist dist\html (
     echo no build file
     exit
 )
-cd %~dp0dist\html & git add . & git commit -m "back --" & git push -f https://github.com/ActingCute/vue-lovely.git cute
+ 
+cp .git %~dp0dist\html && cd %~dp0dist\html & git add . & git commit -m "back --" & git push -f https://github.com/ActingCute/vue-lovely.git cute
